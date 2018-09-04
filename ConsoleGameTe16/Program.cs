5cb3c9d73f38da3872b2d5f275fa019f33c549d0
Tobias Lindberg (Dell) <tobias.lindberg@sodertalje.ntig.se>
2018-09-04 21:16:20 +0200

    En flyttbar spelare.

--- ConsoleGameTe16/Program.cs
+++ ConsoleGameTe16/Program.cs
@@ -8,8 +8,60 @@ namespace ConsoleGameTe16
 {
     class Program
     {
+        static int spelareX = 1;
+        static int spelareY = 1;
+
         static void Main(string[] args)
         {
+            Initialize();
+
+            while (true)
+            {
+                Update();
+                Draw();
+            }
+        }
+
+        static void Initialize()
+        {
+
+        }
+
+        static void Update()
+        {
+            //Läser in tangenttryckningar
+            var key = Console.ReadKey();
+
+            switch(key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    spelareX--;
+                    break;
+                case ConsoleKey.RightArrow:
+                    spelareX++;
+                    break;
+                case ConsoleKey.UpArrow:
+                    spelareY--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    spelareY++;
+                    break;
+            }
+        }
+
+        static void Draw()
+        {
+            Console.Clear();
+            for(int y = 0; y < 10; y++) { 
+                for(int x=0; x < 10; x++)
+                {
+                    if (x == spelareX && y == spelareY)
+                        Console.Write("@");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
